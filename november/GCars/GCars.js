17580d8dd1c62a172139ed24b2f1941cc81875c5
John Doe <johndoe@example.com>
2022-11-03 10:38:38 +0100

    gcars

---
+++ november/GCars/GCars.js
@@ -0,0 +1,9 @@
+const GCars=[
+    {"id":"1","brand":"Audi","hq":"Ingolstadt"},
+    {"id":"2","brand":"BMW","hq":"Munich"},
+    {"id":"3","brand":"Mercedes","hq":"Stuttgart"},
+    {"id":"4","brand":"Opel","hq":"Rüsselsheim"},
+    {"id":"5","brand":"Porsche","hq":"Stuttgart"},
+    {"id":"6","brand":"Volkswagen","hq":"Wolfsburg"},
+    {"id":"7","brand":"Ford","hq":"Dearborn"}
+]
